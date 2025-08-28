1e769e1789a67d55b9ecfde0e19a52084af6f22b
Djakyz <djakyz@gmail.com>
2025-08-28 10:25:31 +0300

    1

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -7,10 +7,15 @@ import { fetchNoteById } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import css from "./NoteDetails.module.css";
+import ErrorMessage from "@/components/ErrorMessage/ErrorMessage";
 
 const NoteDetailsClient = () => {
   const { id } = useParams<{ id: string }>();
-  const { data: note, isLoading } = useQuery({
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
@@ -21,14 +26,20 @@ const NoteDetailsClient = () => {
       <Container>
         {note && (
           <div className={css.container}>
-            <h2 className={css.title}>{note.title}</h2>{" "}
-            <p className={css.content}>{note.content}</p>
-            <div className={css.footer}>
-              <span className={css.tag}>{note.tag}</span>
+            <div className={css.item}>
+              <div className={css.header}>
+                <h2 className={css.title}>{note.title}</h2>
+              </div>
+              <p className={css.content}>{note.content}</p>
+              <p className={css.date}>{note.createdAt}</p>
+              <div className={css.footer}>
+                <span className={css.tag}>{note.tag}</span>
+              </div>
             </div>
           </div>
         )}
         {isLoading && <Loader />}
+        {isError && <ErrorMessage />}
       </Container>
     </Section>
   );
